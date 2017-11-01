392e372bdd0927e596bd03fef2da6c1160680aa3
RichDi <drdr_2@hotmail.com>
2017-10-31 23:23:14 -0600

    make it better

--- home/views.py
+++ home/views.py
@@ -8,61 +8,60 @@ from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
 
-    if request.method == 'POST':      
-        print("entra")
         email = json.loads(request.POST.get('email'))
-        password = json.loads(request.POST.get('password'))     
-        print(email)   
-        print(password)
+        password = json.loads(request.POST.get('password'))
 
         user = authenticate(request, username='Rich', password=password)
         if user is not None:
             login_django(request, user)
             # Redirect to a success page.
             data = {
-               'check': "valido"
+                'check': "valido"
             }
-            print("Valido")    
-            return JsonResponse(data)        
+            print("Valido")
+            return JsonResponse(data)
         else:
             # Return an 'invalid login' error message.
-            print("No valido")            
+            print("No valido")
             data = {
-               'check': "No valido"
+                'check': "No valido"
             }
-            return JsonResponse(data)        
+            return JsonResponse(data)
 
-    template = 'index.html'    
-    context={    	
-    	'title': "PetGurú - Inicio",
+    template = 'index.html'
+    context = {
+        'title': "PetGurú - Inicio",
     }
     return render(request, template, context)
+
 
 def nosotros(request):
-
-    template = 'nosotros.html'    
-    context={    	
-    	'title': "PetGurú - Nosotros",
+    template = 'nosotros.html'
+    context = {
+        'title': "PetGurú - Nosotros",
     }
     return render(request, template, context)
+
 
 def reglamento(request):
-    template = 'reglamento.html'    
-    context={    	
-    	'title': "PetGurú - Reglamento",
+    template = 'reglamento.html'
+    context = {
+        'title': "PetGurú - Reglamento",
     }
     return render(request, template, context)
 
+
 def question(request):
-    template = 'single-question.html'    
-    context={       
+    template = 'single-question.html'
+    context = {
         'title': "PetGurú - Pregúnta",
     }
     return render(request, template, context)
+
 
 @login_required(login_url='home:inicio')
 def logout(request):
     logout_django(request)
     return redirect('home:inicio')
-
