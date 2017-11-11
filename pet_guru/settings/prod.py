4f2b07cc89cc568a6b2a0924761c7a991032b66e
RichDi <drdr_2@hotmail.com>
2017-11-11 17:21:23 -0600

    test: a test space
    add a test space for this

--- pet_guru/settings/prod.py
+++ pet_guru/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+ 
 # Database
 DATABASES = {
     'default': {
