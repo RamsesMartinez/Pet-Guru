b85fcae0b4729249bcd9c97cb123d19617d95642
RichDi <drdr_2@hotmail.com>
2017-11-14 02:51:10 -0600

    Merged develop with chat_logic

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,14 +6,10 @@ import json
 
 @channel_session_user_from_http
 def ws_connect(message):    
-    prefix, label = message['path'].strip('/').split('/')
-    print(label)
+    prefix, label = message['path'].strip('/').split('/')    
     room = Room.objects.get(label=label)    
     Group('chat-' + label).add(message.reply_channel)
-    
-
-    message.reply_channel.send({'accept': True})
-    # Initialise their session
+    message.reply_channel.send({'accept': True})    
     message.channel_session['room'] = room.label    
 
 @channel_session_user_from_http
@@ -21,8 +17,7 @@ def ws_receive(message):
     label = message.channel_session['room']
     room = Room.objects.get(label=label)
     data = json.loads(message['text'])
-    m = room.messages.create(handle=data['handle'], message=data['message'])
-        
+    m = room.messages.create(handle=data['handle'], message=data['message'])        
     Group('chat-'+label).send({'text': json.dumps(m.as_dict())})
 
 @channel_session_user_from_http
